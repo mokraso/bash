e23780457473db41de25e4af4a2c70abe0d0ecce
lai-2 <laidq@outlook.com>
2026-08-26 12:05:21 +0700

    fix (ubuntu-docker): add install agy and codex

--- custom-ubuntu/start.sh
+++ custom-ubuntu/start.sh
@@ -6,6 +6,6 @@ export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
 # docker compose up -d --build
-docker compose up -d
-docker compose exec claude bash
+# docker compose up -d
+docker compose exec cus-ubuntu bash
 
